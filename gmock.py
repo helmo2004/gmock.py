e5eb2b93b28ff37542d2d330357c7276d5908a42
Oliver <Ju>
2019-02-26 09:24:59 +0100

    Change parsing concept
    1. Collect all classes with base classes
    2. Transform them into mock classes
    3. Generate files from mock classes

--- gmock.py
+++ gmock.py
@@ -27,6 +27,143 @@ if sys.version_info < (3, 0):
     def bytes(object, unused = None):
         return __builtin__.bytes(object)
 
+
+class Class:
+    def __init__(self, node):
+        self.__node = node
+        self.__file = str(node.location.file.name)
+        self.__parent_classes = []
+        self.__pure_virtual_methods = []
+        self.__parse_parent_classes()
+        self.__parse_pure_virtual_methods()
+        self.__namespace = self.__parse_name_space()
+        self.__name = str(self.__node.displayname)
+    
+    def __parse_parent_classes(self):
+        for node in self.__node.get_children():
+            if node.kind == CursorKind.CXX_BASE_SPECIFIER:
+                for sub_node in node.get_children():
+                    if sub_node.kind is CursorKind.TYPE_REF: 
+                        self.__parent_classes.append(Class(sub_node.get_definition()))
+    
+    def __parse_pure_virtual_methods(self):
+        for node in self.__node.get_children():
+            if node.kind == CursorKind.CXX_METHOD and node.access_specifier == AccessSpecifier.PUBLIC:
+                if node.is_pure_virtual_method():
+                    self.__pure_virtual_methods.append(node)
+            
+    def get_all_pure_virtual_methods(self):
+        result = []
+        result.extend(self.__pure_virtual_methods)
+        for c in self.__parent_classes:
+            result.extend(c.get_all_pure_virtual_methods())
+        return result
+    
+    def get_all_pure_virtual_methods_as_strings(self):
+        result = []
+        for m in self.get_all_pure_virtual_methods():
+            result.append(str(m.displayname))
+        return result
+    
+    def __parse_name_space(self):
+        result = []
+        node = self.__node.semantic_parent
+        while node is not None:
+            if node.kind in [CursorKind.STRUCT_DECL, CursorKind.CLASS_DECL, CursorKind.NAMESPACE]:
+                result.append(node.displayname)
+            node = node.semantic_parent
+        return list(reversed(result))
+    
+    def get_name_space(self):
+        return "::".join(self.__namespace)
+    
+    def get_file_name(self):
+        return self.__file
+    
+    def get_expr(self):
+        name_space = self.get_name_space()
+        if name_space != "":
+            return name_space + "::" + self.__name
+        else:
+            return self.__name 
+    
+class MockClass:
+    def __init__(self, _class):
+        self.__class = _class
+        self.__mock_methods = self.__generate_mock_methods()
+    
+    def __get_result_type(self, tokens, name):
+        result_type = []
+        for token in tokens:
+            if token in [name, 'operator']:
+                break
+            if token not in ['virtual', 'inline', 'volatile']:
+                result_type.append(token)
+            if token in ['const', 'volatile']:
+                result_type.append(' ')
+        return ''.join(result_type)
+        
+    def __generate_mock_methods(self):
+        result = []
+        for method in self.__class.get_all_pure_virtual_methods():
+            spelling = str(method.spelling)
+            name = str(method.displayname)
+            tokens = [str(token.spelling) for token in method.get_tokens()]
+            result.append(
+                mock_method(
+                    self.__get_result_type(tokens, spelling),
+                    spelling,
+                    method.is_const_method(),
+                    False, # not template class
+                    len(list(method.get_arguments())),
+                    name[len(method.spelling) + 1 : -1]
+                ))
+        return result
+     
+    def get_mock_methods(self):
+        return self.__mock_methods
+    
+    def get_source_class(self):
+        return self.__class
+
+
+class ClassCollector:
+    def __init__(self, file_name, clang_args):
+        self.__file_name = os.path.abspath(file_name)
+        self.__clang_args = clang_args
+        self.encode = 'utf-8'
+        self.expr = ''
+        self.__classes = []
+        self.__parse()
+        
+    def __recursive_parse(self, node):
+        if node.kind in [CursorKind.CLASS_TEMPLATE, CursorKind.STRUCT_DECL, CursorKind.CLASS_DECL]:
+            file = str(node.location.file.name, self.encode)
+            if os.path.abspath(file) != self.__file_name:
+                return
+            self.__classes.append(Class(node=node))
+        else:
+            [self.__recursive_parse(c) for c in node.get_children()]
+    
+    def __parse(self):
+        index = Index.create(excludeDecls = True).parse(
+            path = self.__file_name
+          , args = self.__clang_args
+          , options = TranslationUnit.PARSE_SKIP_FUNCTION_BODIES | TranslationUnit.PARSE_INCOMPLETE
+        )
+        abort = False
+        for d in index.diagnostics:
+            print d
+            if d.severity >= Diagnostic.Error:
+                abort = True
+        if abort:
+            exit(1)
+        self.__recursive_parse(index.cursor)
+    
+    def get_classes(self):
+        return self.__classes                 
+
+
 class mock_method:
     operators = {
         'operator,'   : 'comma_operator',
@@ -136,6 +273,7 @@ class mock_method:
 
         return ''.join(mock)
 
+
 class mock_generator:
     def __is_template_class(self, expr):
         return '<' in expr
@@ -214,84 +352,39 @@ class mock_generator:
             if token == '>':
                 ignore = False
         return ''.join(result)
-
-    def __recursive_traverse(self, node, mock_methods, name_space = "", base_class_collection_mode = False):
-        if node is None: return
-        name = str(node.displayname, self.encode)
-        
-        if node.kind in [CursorKind.CLASS_TEMPLATE, CursorKind.STRUCT_DECL, CursorKind.CLASS_DECL]:
-            print "Class: {} [baseCollectionMode: {}]".format(node.displayname, base_class_collection_mode)
-        
-        if node.kind == CursorKind.CXX_METHOD and node.access_specifier == AccessSpecifier.PUBLIC:
-            spelling = str(node.spelling, self.encode)
-            tokens = [str(token.spelling, self.encode) for token in node.get_tokens()]
-            file = str(node.location.file.name, self.encode)
-            if not base_class_collection_mode and os.path.abspath(file) != self.file_name:
-                return
-            if node.is_pure_virtual_method():
-                print "append function {} in namespace {}".format(name, name_space)
-                if name_space in mock_methods:
-                    mock_methods[name_space][0] = file 
-                mock_methods.setdefault(name_space, [file]).append(
-                    mock_method(
-                         self.__get_result_type(tokens, spelling),
-                         spelling,
-                         node.is_const_method(),
-                         self.__is_template_class(name_space),
-                         len(list(node.get_arguments())),
-                         name[len(node.spelling) + 1 : -1]
-                    )
-                )
-        elif node.kind in [CursorKind.CLASS_TEMPLATE, CursorKind.STRUCT_DECL, CursorKind.CLASS_DECL, CursorKind.NAMESPACE]:
-            if not base_class_collection_mode:
-                name_space = name_space == "" and name or name_space + (name == "" and "" or "::") + name 
-            if name_space.startswith(self.expr):
-                [self.__recursive_traverse(c, mock_methods, name_space, base_class_collection_mode) for c in node.get_children()]
-        else:
-            [self.__recursive_traverse(c, mock_methods, name_space, base_class_collection_mode) for c in node.get_children()]
-        if node.kind == CursorKind.CXX_BASE_SPECIFIER:
-            file = str(node.location.file.name, self.encode)
-            if os.path.abspath(file) != self.file_name and not base_class_collection_mode:
-                return
-            print "   Bases [{}]".format(", ".join([subnode.displayname for subnode in node.get_children()]))
-            for sub_node in node.get_children():
-                if sub_node.kind is CursorKind.TYPE_REF: 
-                    self.__recursive_traverse(c.get_definition(), mock_methods, name_space, True)
-
+    
     def __get_mock_methods(self, node, mock_methods, name_space = ""):
         self.__recursive_traverse(node, mock_methods, name_space, False)
-                           
 
-    def __generate_file(self, expr, mock_methods, file_type, file_template_type):
+    def __generate_file(self, mock_class, file_type, file_template_type):
+        source_class = mock_class.get_source_class()
+        expr = source_class.get_expr()
         interface = self.__get_interface(expr)
         mock_file = {
             'hpp' : self.mock_file_hpp % { 'interface' : interface },
             'cpp' : self.mock_file_cpp % { 'interface' : interface },
         }
         path = self.path + "/" + mock_file[file_type]
+        print "Generating {}".format(path)
+        source_file_name = source_class.get_file_name()
         not os.path.exists(os.path.dirname(path)) and os.makedirs(os.path.dirname(path))
+        content = file_template_type % {
+            'mock_file_hpp' : mock_file['hpp'],
+            'mock_file_cpp' : mock_file['cpp'],
+            'generated_dir' : self.path,
+            'guard' : "__{:X}".format(time_stamp) + "_"+ mock_file[file_type].replace('.', '_').upper(),
+            'dir' : os.path.dirname(source_file_name),
+            'file' : os.path.basename(source_file_name),
+            'namespaces_begin' : self.__pretty_namespaces_begin(expr),
+            'interface' : interface,
+            'template_interface' : expr.split("::")[-1],
+            'template' : self.__pretty_template(expr),
+            'mock_methods' : self.__pretty_mock_methods(mock_class.get_mock_methods()),
+            'namespaces_end' : self.__pretty_namespaces_end(expr)
+            }
         with open(path, 'w') as file:
-            file.write(file_template_type % {
-                'mock_file_hpp' : mock_file['hpp'],
-                'mock_file_cpp' : mock_file['cpp'],
-                'generated_dir' : self.path,
-                'guard' : "__{:X}".format(time_stamp) + "_"+ mock_file[file_type].replace('.', '_').upper(),
-                'dir' : os.path.dirname(mock_methods[0]),
-                'file' : os.path.basename(mock_methods[0]),
-                'namespaces_begin' : self.__pretty_namespaces_begin(expr),
-                'interface' : interface,
-                'template_interface' : expr.split("::")[-1],
-                'template' : self.__pretty_template(expr),
-                'mock_methods' : self.__pretty_mock_methods(mock_methods[1:]),
-                'namespaces_end' : self.__pretty_namespaces_end(expr)
-            })
+            file.write(content)
 
-    def __parse(self, file_name, args):
-        return Index.create(excludeDecls = True).parse(
-            path = file_name
-          , args = args
-          , options = TranslationUnit.PARSE_SKIP_FUNCTION_BODIES | TranslationUnit.PARSE_INCOMPLETE
-        )
 
     def __init__(self, file_name, args, expr, path, mock_file_hpp, file_template_hpp, mock_file_cpp, file_template_cpp, encode = "utf-8"):
         self.expr = expr
@@ -302,29 +395,16 @@ class mock_generator:
         self.file_template_cpp = file_template_cpp
         self.encode = encode
         self.file_name = os.path.abspath(file_name)
-        self.class_file_map = {}
-        result = self.__parse(file_name, args)
-        abort = False
-        for d in result.diagnostics:
-            print d
-            if d.severity >= Diagnostic.Error:
-                abort = True
-        self.cursor = result.cursor
-        if abort:
-            exit(1)
-        
+        classes = ClassCollector(file_name=file_name, clang_args=args).get_classes()
+        self.__mock_classes = [MockClass(c) for c in classes]
 
     def generate(self):
-        
-        mock_methods = {}
-        self.__get_mock_methods(self.cursor, mock_methods)
-        for expr, mock_methods in mock_methods.items():
-            print "{}:\n  [{}]".format(expr, ", ".join([str(m) for m in mock_methods]))
-            if len(mock_methods) > 0:
-                if os.path.abspath(self.file_name) != os.path.abspath(mock_methods[0]):
-                    continue
-                self.file_template_hpp != "" and self.__generate_file(expr, mock_methods, "hpp", self.file_template_hpp)
-                self.file_template_cpp != "" and self.__generate_file(expr, mock_methods, "cpp", self.file_template_cpp)
+        for mock_class in self.__mock_classes:
+            if len(mock_class.get_mock_methods()) > 0:
+                source_class = mock_class.get_source_class()
+                print "Found Class '{}' with following pure virtual methods: [{}]".format(source_class.get_expr(), "; ".join(source_class.get_all_pure_virtual_methods_as_strings()))
+                self.file_template_hpp != "" and self.__generate_file(mock_class, "hpp", self.file_template_hpp)
+                self.file_template_cpp != "" and self.__generate_file(mock_class, "cpp", self.file_template_cpp)
         return 0
 
 def main(args):
